dad0cc1148718050ae20fb4fb6b1076b93e3daf4
KG-DI-KXNG <kevandogordon@yahoo.com>
2021-10-10 13:15:38 -0500

    Form updates

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit.prevent="submit">
     @csrf
     {{-- <input hidden name="surveyId" value="{{ $survey->id }}"> --}}
-    <div class="card">
+    <div class="card mb-4">
         <div class="card-header bg-white p-4">
             <h2 class="mb-0">
                 <label for="">Survey Name: {{$surveyName}}</label> 
@@ -14,42 +14,64 @@
         <div class="col-span-6 sm:col-span-6 mt-2">
             Question:{{ $i+1 }}
             <fieldset>
-              <legend class="text-base font-medium text-gray-700"><input name="c{{$i}}" wire:model="question.{{$i}}" class="w-72" placeholder="What would you like to see improved?"></legend>
-              <p class="text-sm text-gray-500">Check all that apply</p>
+              <legend class="text-base font-medium text-gray-700"><input wire:model="question.{{$i}}" class="w-72" placeholder="What would you like to see improved?"></legend>
+              <p class="text-sm text-gray-500">Check all that apply in 
+                <select name="" id="" wire:model="type.{{$i}}" >
+                  <option value="sa">Short Answer</option>
+                  <option value="tf">True Or False</option>
+                  <option value="mc">Multiple Choices</option>
+                </select></p>
+              
               <div class="mt-4 space-y-4">
+                @if ($type[$i] === "mc")
 
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input id="comments" name="comments" type="checkbox" value="FE"
+                    <input name="{{$type[$i].$i}}" type="radio">
+                    <label for="comments" class="font-medium text-gray-700"><input type="text" value="Full-stack" placeholder="Answer #1" wire:model="options.{{$i}}.1.value"></label>
+                    <br>
+                    <input name="{{$type[$i].$i}}" type="radio">
+                    <label for="comments" class="font-medium text-gray-700"><input type="text" value="Front-End" placeholder="Answer #2" wire:model="options.{{$i}}.2.value"></label>
+                    <br>
+          
+                    <input name="{{$type[$i].$i}}" type="radio">
+                    <label for="comments" class="font-medium text-gray-700"><input type="text" value="Back-End" placeholder="Answer #3" wire:model="options.{{$i}}.3.value"></label>
+                    <br>
+                        
+                    <input name="{{$type[$i].$i}}" type="radio">
+                    <label class="font-medium text-gray-700"><input type="text" value="Lamp-Stack" placeholder="Answer #4" wire:model="options.{{$i}}.4.value"></label>
+                    
+                    @endif
+                @if ($type[$i] === "sa")
+
+                    {{-- <input id="sa" name="short_answer"  type="text"
+                      class="border-gray-300 rounded"> --}}
+                 
+              {{  $type[$i]}}
+           
+              <input type="text" name="{{$type[$i].$i}}" id="">
+                
+                @endif
+                @if ($type[$i] === "tf")
+
+{{--              
+                    <input id="tf" name="tf" type="radio" value="true"
                       class="h-4 w-4 border-gray-300 rounded">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="comments" class="font-medium text-gray-700">Front-end</label>
+                  
+                    <label for="tf" class="font-medium text-gray-700">Front-end</label>
                     <p class="text-gray-500">Things like HTML, CSS, JS or React</p>
-                  </div>
-                </div>
-
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input id="comments" name="comments" type="checkbox" value="BE"
+                  
+               
+                    <input id="tf" name="tf" type="radio" value="false"
                       class="h-4 w-4 border-gray-300 rounded">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="comments" class="font-medium text-gray-700">Back-end</label>
-                    <p class="text-gray-500">APIs and Microservices with NodeJS, Express</p>
-                  </div>
-                </div>
+            
+                    <label for="tf" class="font-medium text-gray-700">Back-end</label>
+                    <p class="text-gray-500">APIs and Microservices with NodeJS, Express</p> --}}
+                  
+                {{$type[$i]}}
+                <input name="{{$type[$i].$i}}" type="radio">
+                <input name="{{$type[$i].$i}}" type="radio">
 
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input id="comments" name="comments" type="checkbox" value="FS"
-                      class="h-4 w-4 border-gray-300 rounded">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="comments" class="font-medium text-gray-700">Full-stack</label>
-                    <p class="text-gray-500">The complete Stack for an End-to-End understanding</p>
-                  </div>
-                </div>
+                @endif
+                
               </div>
             </fieldset>
           </div>

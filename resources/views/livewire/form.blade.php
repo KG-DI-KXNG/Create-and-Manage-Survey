73e6de3dbc678d0a4b054f36244db9e7cf75aaa9
Olivia Hales <halesolivia7@gamil.com>
2021-10-11 10:40:38 -0500

    Update design form blade

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,20 +1,20 @@
-<form class="col-span-3 mx-72" wire:submit.prevent="submit">
+<form class="col-span-3 mx-60" wire:submit.prevent="submit">
     @csrf
     {{-- <input hidden name="surveyId" value="{{ $survey->id }}"> --}}
-    <div class="px-10 bg-gray-100">
+    <div class="px-10 bg-gray-100 mb-4">
       <div class="mt-2 mb-4">
-          <h1 class="text-xl md:text-xl flex-justify">
-              <p class="text-center font-bold text-black" for="">Survey Name: <p class="text-black text-center font-bold text-green-400">{{$surveyName}}</p></p> 
+          <h1 class="flex-justify">
+              <p class="text-md sm:text-md text-center text-black" for="">Survey Name: <p class="text-xl md:text-xl text-black text-center font-bold text-green-400">{{$surveyName}}</p></p> 
           </h1>
       </div>
      
        
         @for ($i=0;$i<$sectionNo;$i++)
-        <div class="col-span-6 md:col-span-6 mt-4 bg-white rounded-md p-8 shadow-md">
-            Question:{{ $i+1 }}
-            <fieldset>
-              <legend class="text-base font-medium sm:font-medium text-gray-700"><input wire:model="question.{{$i}}" class="w-80 sm:w-80" placeholder="What would you like to see improved?"></legend>
-              <p class="text-sm text-gray-500">Select the type of question: 
+        <div class="col-span-6 md:col-span-6 mt-2 mb-8 bg-white rounded-md py-8 px-4 shadow-md text-md">
+            <h2 class="text-green-500 text-xl mb-4 py-2">Question:{{ $i+1 }}
+            <fieldset class="w-full">
+              <legend class="text-base text-lg md:text-md text-gray-700 w-full mb-2"><input wire:model="question.{{$i}}" class="w-full mt-2 hover:bg-blue-100 focus:outline-none" placeholder="What would you like to see improved?"></legend>
+              <p class="text-lg md:text-md text-gray-500 w-full">Select the type of question: 
                 <select name="" id="" wire:model="type.{{$i}}" required >
                   <option selected>--Select Option --</option>
                   <option value="sa">Short Answer</option>
@@ -25,29 +25,26 @@
                 @if ($type[$i] === "mc")
 
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700 sm:font-medium"><input type="text" value="Full-stack" placeholder="Answer #1" wire:model="options.{{$i}}.1.value"></label>
+                    <label for="comments" class="font-medium text-gray-700 sm:font-medium"><input class="focus:outline-none" type="text" value="Full-stack" placeholder="Answer #1" wire:model="options.{{$i}}.1.value"></label>
                     <br>
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700"><input type="text" value="Front-End" placeholder="Answer #2" wire:model="options.{{$i}}.2.value"></label>
+                    <label for="comments" class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Front-End" placeholder="Answer #2" wire:model="options.{{$i}}.2.value"></label>
                     <br>
           
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700"><input type="text" value="Back-End" placeholder="Answer #3" wire:model="options.{{$i}}.3.value"></label>
+                    <label for="comments" class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Back-End" placeholder="Answer #3" wire:model="options.{{$i}}.3.value"></label>
                     <br>
                         
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label class="font-medium text-gray-700"><input type="text" value="Lamp-Stack" placeholder="Answer #4" wire:model="options.{{$i}}.4.value"></label>
+                    <label class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Lamp-Stack" placeholder="Answer #4" wire:model="options.{{$i}}.4.value"></label>
                     
                     @endif
               
                 @if ($type[$i] === "sa")
 
                     {{-- <input id="sa" name="short_answer"  type="text"
-                      class="border-gray-300 rounded"> --}}
-                 
-              {{  $type[$i]}}
-           
-              <input type="text" name="{{$type[$i].$i}}" id="">
+                      class="border-gray-300 rounded"> --}}          
+                  <input class="focus:outline-none border-b w-full text-gray-700 p-4" type="text" name="{{$type[$i].$i}}" id="" placeholder="Your Answer">
                 
                 @endif
               
@@ -65,7 +62,8 @@
 
         @endfor
 
-                <button class="bg-green-500 p-2 rounded-md mt-4" wire:submit="submit">Submit</button>
+                <button class="bg-green-500 p-2 rounded-md mt-4 w-1/4" wire:submit="submit">Submit</button>
+                <button class="bg-green-500 p-2 rounded-md mt-4 w-1/4" wire:submit="submit">Cancel</button>
             
     </div>
 </form>

aa0f0a70bcc253f1cd44f72f949c2a82cb2edd72
Olivia Hales <halesolivia7@gamil.com>
2021-10-11 15:28:23 -0500

    update design

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -13,7 +13,7 @@
         <div class="col-span-6 md:col-span-6 mt-2 mb-8 bg-white rounded-md py-8 px-4 shadow-md text-md">
             <h2 class="text-green-500 text-xl mb-4 py-2">Question:{{ $i+1 }}
             <fieldset class="w-full">
-              <legend class="text-base text-lg md:text-md text-gray-700 w-full mb-2"><input wire:model="question.{{$i}}" class="w-full mt-2 hover:bg-blue-100 focus:outline-none" placeholder="What would you like to see improved?"></legend>
+              <legend class="text-base text-lg md:text-md text-gray-700 w-full mb-2"><input wire:model="question.{{$i}}" class="w-full mt-2 hover:bg-blue-100" placeholder="What would you like to see improved?"></legend>
               <p class="text-lg md:text-md text-gray-500 w-full">Select the type of question: 
                 <select name="" id="" wire:model="type.{{$i}}" required >
                   <option selected>--Select Option --</option>
@@ -25,26 +25,22 @@
                 @if ($type[$i] === "mc")
 
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700 sm:font-medium"><input class="focus:outline-none" type="text" value="Full-stack" placeholder="Answer #1" wire:model="options.{{$i}}.1.value"></label>
+                    <label for="comments" class="font-medium text-gray-700 sm:font-medium"><input class="" type="text" value="Full-stack" placeholder="Answer #1" wire:model="options.{{$i}}.1.value"></label>
                     <br>
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Front-End" placeholder="Answer #2" wire:model="options.{{$i}}.2.value"></label>
+                    <label for="comments" class="font-medium text-gray-700"><input class="" type="text" value="Front-End" placeholder="Answer #2" wire:model="options.{{$i}}.2.value"></label>
                     <br>
           
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label for="comments" class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Back-End" placeholder="Answer #3" wire:model="options.{{$i}}.3.value"></label>
+                    <label for="comments" class="font-medium text-gray-700"><input class="" type="text" value="Back-End" placeholder="Answer #3" wire:model="options.{{$i}}.3.value"></label>
                     <br>
                         
                     <input name="{{$type[$i].$i}}" type="radio">
-                    <label class="font-medium text-gray-700"><input class="focus:outline-none" type="text" value="Lamp-Stack" placeholder="Answer #4" wire:model="options.{{$i}}.4.value"></label>
+                    <label class="font-medium text-gray-700"><input class="" type="text" value="Lamp-Stack" placeholder="Answer #4" wire:model="options.{{$i}}.4.value"></label>
                     
                     @endif
               
                 @if ($type[$i] === "sa")
-
-                    {{-- <input id="sa" name="short_answer"  type="text"
-                      class="border-gray-300 rounded"> --}}          
-                  <input class="focus:outline-none border-b w-full text-gray-700 p-4" type="text" name="{{$type[$i].$i}}" id="" placeholder="Your Answer">
                 
                 @endif
               
@@ -59,11 +55,13 @@
               
             </fieldset>
     </div>
-
         @endfor
 
-                <button class="bg-green-500 p-2 rounded-md mt-4 w-1/4" wire:submit="submit">Submit</button>
-                <button class="bg-green-500 p-2 rounded-md mt-4 w-1/4" wire:submit="submit">Cancel</button>
-            
+        <div class="flex justify-between">
+          <button class="bg-green-500 hover:bg-green-700 text-white p-2 rounded-md mt-4 w-1/4" wire:submit="submit">Create</button>
+          <button class="text-black hover:text-red-500 pr-4" wire:submit="#">Cancel</button>
+          
+        </div>
+      
     </div>
 </form>
